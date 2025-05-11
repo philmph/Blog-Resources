029bb204811a140fb232cd3120d8dc8cc94f4deb
Philipp Maier <25420070+philmph@users.noreply.github.com>
2025-05-11 19:26:03 +0200

    Move yaml transformation to resource block

--- posts/20250509_opentofu-<3-yaml/locals.tf
+++ posts/20250509_opentofu-<3-yaml/locals.tf
@@ -2,6 +2,4 @@ locals {
   yaml_path     = "${path.root}/configuration"
   yaml_filename = "dns_records.yaml"
   yaml_data     = yamldecode(file("${local.yaml_path}/${local.yaml_filename}"))
-
-  transformed_yaml = { for i, o in local.yaml_data.dns_records : o.name => o }
 }
--- posts/20250509_opentofu-<3-yaml/main.tf
+++ posts/20250509_opentofu-<3-yaml/main.tf
@@ -1,6 +1,10 @@
+locals {
+  dns_records_from_yaml = { for i, o in local.yaml_data.dns_records : o.name => o }
+}
+
 # https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/dns_record
 resource "cloudflare_dns_record" "this" {
-  for_each = local.transformed_yaml
+  for_each = local.dns_records_from_yaml
 
   zone_id = var.zone_id
 
