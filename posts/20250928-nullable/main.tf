a989547154e6c9d7e1dd2c36d2056bcd8b41547c
Philipp Maier <25420070+philmph@users.noreply.github.com>
2025-09-30 22:10:43 +0200

    Add showcase for loops

--- posts/20250928-nullable/main.tf
+++ posts/20250928-nullable/main.tf
@@ -12,3 +12,28 @@ module "basic_usage" {
 output "basic_usage" {
   value = module.basic_usage
 }
+
+# Loop example
+variable "loop" {
+  type = map(object({
+    not_nullable_with_default    = optional(string)
+    not_nullable_without_default = optional(string)
+    nullable_with_default        = optional(string)
+    nullable_without_default     = optional(string)
+  }))
+}
+
+module "loop" {
+  source = "./modules/nullable"
+
+  for_each = var.loop
+
+  not_nullable_with_default    = each.value.not_nullable_with_default
+  not_nullable_without_default = each.value.not_nullable_without_default
+  nullable_with_default        = each.value.nullable_with_default
+  nullable_without_default     = each.value.nullable_without_default
+}
+
+output "loop" {
+  value = module.loop
+}
